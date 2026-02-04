526eb035bc0a0d71d66b77bcfaa7dae640619f03
Shreya <shreyavnadiger@gmail.com>
2026-02-04 15:54:51 +0530

    hoj

--- aa.py
+++ aa.py
@@ -1,2 +1,3 @@
 def hello():
     print("Hello, world!")
+    print("hello 4MH23CS146")
--- app.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello World")
-    print("hello 4MH23CS146")
-
-if __name__ == "__main__":
-    main()
